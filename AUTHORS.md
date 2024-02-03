796c580aabf9888816a0a421f14d5bd4636cb384
KEERTHANAUDAY <u.keerthana@gmail.com>
2024-02-03 14:46:55 +0100

    initial commit

---
+++ AUTHORS.md
@@ -0,0 +1,3 @@
+# Authors
+
+- keerthana,viki <<u.keerthana@gmail.com>> [@KEERTHANAUDAY](https://github.com/KEERTHANAUDAY)
---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import compas
+import lca_miro
+import math
+import numpy
+
+
+def pytest_ignore_collect(path):
+    if "rhino" in str(path):
+        return True
+
+    if "blender" in str(path):
+        return True
+
+    if "ghpython" in str(path):
+        return True
+
+
+@pytest.fixture(autouse=True)
+def add_compas(doctest_namespace):
+    doctest_namespace["compas"] = compas
+
+
+@pytest.fixture(autouse=True)
+def add_lca_miro(doctest_namespace):
+    doctest_namespace["lca_miro"] = lca_miro
+
+
+@pytest.fixture(autouse=True)
+def add_math(doctest_namespace):
+    doctest_namespace["math"] = math
+
+
+@pytest.fixture(autouse=True)
+def add_np(doctest_namespace):
+    doctest_namespace["np"] = numpy
