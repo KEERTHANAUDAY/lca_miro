4e396de2c70d1f117a95b6570e2b8ca47bcdb060
KEERTHANAUDAY <u.keerthana@gmail.com>
2024-02-03 21:59:14 +0100

    convert to compas mesh

---
+++ data/rhino_test2.py
@@ -0,0 +1,14 @@
+import compas
+from compas_rhino.conversions import mesh_to_compas
+import scriptcontext as sc
+import rhinoscriptsyntax as rs
+
+mesh_guid = rs.GetObject("Select mesh", rs.filter.mesh)
+
+obj = sc.doc.Objects.Find(mesh_guid)
+mesh = mesh_to_compas(obj.Geometry)
+
+print(mesh)
+
+
+
--- src/lca_miro/datastructures/panel.py
+++ src/lca_miro/datastructures/panel.py
@@ -20,6 +20,22 @@ class Panel(Mesh):
         with open(file_path, "w") as json_file:
             json.dump(self.json_file, json_file)
 
+    def from_rhinomesh(cls, obj):
+        """Class method for constructing a panel from a Rhino mesh.
+        Parameters
+        ----------
+        guid : str
+            The GUID of the mesh.
+        Returns
+        -------
+        Panel
+            The block corresponding to the Rhino mesh.
+        """
+        from compas_rhino.conversions import mesh_to_compas
+
+        mesh = mesh_to_compas(obj)
+        return mesh.to_compas(cls)
+
 
 if __name__ == "__main__":
     panel = Panel()
