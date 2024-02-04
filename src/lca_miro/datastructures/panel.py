63ab1044ae561b47344070a0e7aa79c839d623ed
KEERTHANAUDAY <u.keerthana@gmail.com>
2024-02-04 09:18:48 +0100

    panel update

--- src/lca_miro/datastructures/panel.py
+++ src/lca_miro/datastructures/panel.py
@@ -9,19 +9,29 @@ import json
 class Panel(Mesh):
     def __init__(self):
         super(Panel, self).__init__()
-        self.json_file = None
+        self.json_object = None
         self.name = None
+        self.material_type = None
 
-    def load_json(self, file_path):
+    # @property
+    # def data(self):
+    #     if self.name is None: self.update_name()
+    #     return self.name
+
+    # json stuff
+    def load_data_json(self, file_path):
         with open(file_path, "r") as json_file:
-            data = json.load(json_file)
-            self.json_file = data
-            self.material.name = None
+            self.json_object = json.load(json_file)
+            self.name = self.json_object["name"]
+            self.material_type = self.json_object["material_type"]
+
+            # self.json_file = data
 
     def save_file(self, file_path):
         with open(file_path, "w") as json_file:
             json.dump(self.json_file, json_file)
 
+    # load rhino as panel mesh
     def from_rhinomesh(cls, obj):
         """Class method for constructing a panel from a Rhino mesh."""
         from compas_rhino.conversions import mesh_to_compas
@@ -29,18 +39,17 @@ class Panel(Mesh):
         mesh = mesh_to_compas(obj)
         return mesh
 
-    def set_material_name(self):
-        if "name" in self.json_file:
-            self.material_name = self.json_file["material_name"]
+    # embodied carbon
+    # get the material quantity of the assembly
+    # multiply with mesh surface area
+    # update face centroid with the attribute
 
 
 if __name__ == "__main__":
     import os
 
-    HERE = os.path.dirname(__file__)
-    FILE_I = os.path.join(HERE, "jsontest00.json")
     panel = Panel()
-    panel.load_json(FILE_I)
-    json_data = panel.json_file
-    name = panel.name
-    print(name)
+    panel.load_data_json("C:/Users/ukeer/GitHub/lca_miro/data/jsontest00.json")
+    print(panel.name)
+    print(panel.json_object)
+    print(panel.material_type)
